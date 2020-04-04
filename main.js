7de84b88ad50655072a8b24aaa2e3fbc1fc6a4e8
nam dinh <namndwebdev@gmail.com>
2020-04-04 16:23:43 +0700

    add chat

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8888
-const duongDan = require('path')
-
-app.use('/open', express.static(duongDan.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-    res.send('Welcome')
-})
-
-app.get('/home', (req, res) => {
-    res.send('HOMPAGEE')
-})
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000
 
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/chat.html');
+});
 
-app.get('/login', (req, res) => {
-    var duongDanAbsolute = duongDan.join(__dirname, 'index.html')
-    console.log(duongDanAbsolute);
-    res.sendFile(duongDanAbsolute)
-})
+io.on('connection', function(socket){
+    console.log('a user connected');
+    // khi client disconnect. , tat tab chorme di
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+
+        socket.broadcast.emit('server_reply', msg)
+    });
+
+
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
