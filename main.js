62bf156926056106e8498ac03d0dd33cbd3b2f9e
nam dinh <namndwebdev@gmail.com>
2020-04-04 14:17:31 +0700

    tao server nodejs

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-String 
-Number
-Boolean
-Array
-Object
-function
-null
-undefined
+const express = require('express')
+const app = express()
+const port = 3000
+const duongDan = require('path')
 
-''
-0 NaN
-false
-null
-undefined
+app.use('/open', express.static(duongDan.join(__dirname, 'public')))
 
-! +-*/  += == ===
-function test(paramter){
-    
-}
+// http://localhost:3000/
+app.get('/', (req, res) => {
+    res.send('Welcome')
+})
+
+app.get('/home', (req, res) => {
+    res.send('HOMPAGEE')
+})
+
+
+
+app.get('/login', (req, res) => {
+    var duongDanAbsolute = duongDan.join(__dirname, 'index.html')
+    console.log(duongDanAbsolute);
+    res.sendFile(duongDanAbsolute)
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
